eb35f8e2c88e836dc4668797cf8de4ce549e18a5
shovan777 <raj.shrestha778@gmail.com>
2023-08-24 15:32:58 +0545

    Import profile entities in profile module.

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
 import { ProfilesResolver } from './profiles.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile, ProfileImage } from './entities/profile.entity';
 
 @Module({
-  providers: [ProfilesResolver, ProfilesService]
+  providers: [ProfilesResolver, ProfilesService],
+  imports: [TypeOrmModule.forFeature([Profile, ProfileImage])],
 })
 export class ProfilesModule {}
